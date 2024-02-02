a2fe5bc50cd2ede4f9cfe185f5e8f14cfcd7f656
Anurag Ambekar <anuragambekar@Anurags-MacBook-Pro.local>
2024-02-03 03:25:45 +0530

    Basic http server with express.

---
+++ week2/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const app = express()
+const port = 3000
+
+const calSum = (num) => {
+    var sum = 0
+    for (var i = 0; i < num; i++) {
+        sum += i
+    }
+    return sum
+}
+
+function callbackFunc(req, resp) {
+    const total = calSum(100)
+    const res = `Ans is ${total}`
+    resp.send(res)
+}
+
+app.get('/', callbackFunc)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
