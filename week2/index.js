23205a500b39bec925839c480abd14737ead75a2
Anurag Ambekar <anuragambekar@Anurags-MacBook-Pro.local>
2024-02-03 12:09:18 +0530

    Basic http server with express.

--- week2/index.js
+++ week2/index.js
@@ -5,18 +5,21 @@ const port = 3000
 
 const calSum = (num) => {
     var sum = 0
-    for (var i = 0; i < num; i++) {
+    for (var i = 0; i <= num; i++) {
         sum += i
     }
     return sum
 }
 
 function callbackFunc(req, resp) {
-    const total = calSum(100)
+    //Receving data from client using QUERY PARAM
+    const num = req.query.num
+    const total = calSum(num)
     const res = `Answer is ${total}`
     resp.send(res)
 }
 
+//Listen to incoming requests from client on port 3000
 app.get('/Answer', callbackFunc)
 
 app.listen(port, () => {
