5fa6dfe10a7310d9c2731c51ece5ff19029322b0
Anurag Ambekar <anuragambekar1997@gmail.com>
2024-02-04 01:58:25 +0530

    sending resp as html file

--- week2/index.js
+++ week2/index.js
@@ -9,8 +9,9 @@ function middleware1(req, res, next) {
     numOfreq += 1
     console.log("from inside middleware ", req.headers.num)
     console.log(`number of request - ${numOfreq}`)
-    if (req.headers.num || req.query.num || req.body.numgit) next()
-    else res.send('bad request')
+    // if (req.headers.num || req.query.num || req.body.numgit) next()
+    // else res.send('bad request')
+    next()
 }
 
 //Intercepts request comming from clients.
@@ -28,6 +29,12 @@ const calSum = (num) => {
     return sum
 }
 
+function sendRespObj(ans) {
+    return {
+        "total": ans
+    }
+}
+
 function callbackFunc(req, resp) {
     //Receving data from client using QUERY PARAM
     const num = req.query.num
@@ -40,12 +47,40 @@ function callbackFunc(req, resp) {
     console.log(num2)
     const total = calSum(num3 || num2 || num)
     const res = `Answer is ${total}`
-    resp.send(res)
+    const body = sendRespObj(total)
+    // resp.status(200).send(body)
+
+    //Directly send html file as response.
+    resp.sendFile(__dirname + "/index.html")
+
+    //cannot send string in .json() can send json .send()
+    // resp.json(body)
+}
+
+function getHtmlPage(req, res) {
+    res.send(`<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <h1>Hello from HTML</h1>
+</body>
+
+</html>`)
 }
 
 //Listen to incoming requests from client on port 3000 (*request handlers*)
+app.get('/', getHtmlPage)
 app.get('/Answer', callbackFunc)
-app.post('/', callbackFunc)
+app.post('/Total', callbackFunc)
 
 
 app.listen(port, () => {
