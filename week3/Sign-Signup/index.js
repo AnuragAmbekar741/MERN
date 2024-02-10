b7b3042db982da7a78715514145679104fd85b56
Anurag Ambekar <anuragambekar1997@gmail.com>
2024-02-10 17:39:46 +0530

    week3 sign in assign ment

--- week3/Sign-Signup/index.js
+++ week3/Sign-Signup/index.js
@@ -19,4 +19,18 @@ app.get('/signup', (req, resp) => {
     resp.sendFile(path.join(__dirname + "/sign-up.html"))
 })
 
+app.post('/signup', (req, resp) => {
+    const { email, password } = req.body
+    if (!email || !password) return
+    users.push({ email: email, password: password })
+    console.log(email, password)
+    resp.json({ message: "user added successfully" })
+})
+
+app.post('/signin', (req, resp) => {
+    const { username, password, confirmpass } = req.body
+    users.push({ username: username, password: password })
+    resp.json({ message: "user added successfully" })
+})
+
 app.listen(port, () => console.log(`server running on ${port}`))
