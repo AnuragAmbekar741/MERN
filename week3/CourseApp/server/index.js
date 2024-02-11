ca59aae3a35d6facf7bcf61052dc768f6fbb3e56
Anurag Ambekar <anuragambekar1997@gmail.com>
2024-02-11 14:52:39 +0530

    Course app server - db connected

--- week3/CourseApp/server/index.js
+++ week3/CourseApp/server/index.js
@@ -1,9 +1,14 @@
 const express = require("express")
-const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require('dotenv').config();
+
 
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json())
 
+mongoose.connect(process.env.MONGOOSE_URL, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "CourseApp" })
 app.listen(port, () => console.log(`Server running on port ${port}`))
