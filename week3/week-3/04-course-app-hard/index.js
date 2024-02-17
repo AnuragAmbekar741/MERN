05e3412cc7fca028896609a1e0d4a72b5ad74dcf
Anurag Ambekar <anuragambekar1997@gmail.com>
2024-02-17 20:13:29 +0530

    04 hard assign - admin routes complete

--- week3/week-3/04-course-app-hard/index.js
+++ week3/week-3/04-course-app-hard/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const jwt = require("jsonwebtoken")
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
 
 const userSchema = new mongoose.Schema({
   username: String,
@@ -29,17 +31,52 @@ const user = mongoose.model("User", userSchema)
 const admin = mongoose.model("Admin", adminSchema)
 const course = mongoose.model("Course", courseSchema)
 
-// Admin routes
-app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+mongoose.connect(process.env.MONGOOSE_URL, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader) return res.status(403).json({ message: 'Invalid request' })
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Invalid request' })
+    if (user) {
+      req.user = user
+      next()
+    }
+  })
+}
+
+
+app.post('/admin/signup', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) return res.status(400).json({ message: "Username and password are required." });
+  try {
+    const adminExist = await admin.findOne({ username: username });
+    if (adminExist) return res.status(403).json({ message: "Admin already exists." });
+    const newAdmin = new admin({ username, password });
+    await newAdmin.save();
+    const token = jwt.sign({ username, role: 'admin' }, process.env.SECRET_KEY, { expiresIn: '1h' })
+    return res.status(201).json({ message: "Admin created successfully.", token: token });
+  } catch (error) {
+    console.error("Error occurred during admin signup:", error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+app.post('/admin/login', async (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) return res.status(400).json({ message: "Username and password are required." });
+  const user = await admin.findOne({ username, password })
+  if (!user) return res.status(400).json({ message: "Invalid username or password" });
+  const token = jwt.sign({ username, role: 'admin' }, process.env.SECRET_KEY, { expiresIn: "1h" })
+  return res.json({ message: "Admin logged in successfully.", token });
 });
 
-app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+app.post('/admin/courses', authenticateJwt, async (req, res) => {
+  const newCourse = req.body
+  const addCourse = new course(newCourse)
+  await addCourse.save()
+  res.json({ message: "New course added", courseId: newCourse.id })
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
